f3a70ec9f28b38aa46dfe505f968341b1694f4e4
Nayeem0011 <sknayeemislam384@gmail.com>
2026-05-18 16:43:23 +0600

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Powerful from './component/ourPowerful/Powerful'
 import HeroSection from './component/heroSection/HeroSection'
+import Footer from './component/footer/footer'
 
 function App() {
-
 
   return (
     <div className=''>
       <HeroSection />
       <Powerful />
+      <Footer />
     </div>
   )
 }
